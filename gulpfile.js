f001dfb50b2bc5659fd3f492166316e81e0e5b04
xiabingyi <876186634@qq.com>
2018-11-29 00:00:00 +0800

    gulp imagemin/cssmin learn

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require("gulp");
 var htmlmin = require("gulp-htmlmin");
 
-
 gulp.task('testhtmlmin', function () {
     var options = {
         removeComments: true,
@@ -16,4 +15,37 @@ gulp.task('testhtmlmin', function () {
     gulp.src('src/html/*.html')
         .pipe(htmlmin(options))
         .pipe(gulp.dest('dist/html'));
+});
+
+var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
+    pngquant = require("imagemin-pngquant");
+
+gulp.task('testimagemin',function(){
+    var options = {
+        optimizationLevel: 5,
+            progressive: true,
+            interlaced: true,
+            multipass: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+    };
+    gulp.src('src/img/*.{png,jpg,gif,ico}')
+        .pipe(imagemin(options))
+        .pipe(gulp.dest('dist/img'));
+});
+
+var gulp = require("gulp"),
+var cssmin = require("gulp-clean-css");
+
+gulp.task('testcssmin',function(){
+    gulp.src('src/css/*.css')
+        .pipe(cssmin({
+            advanced: false,
+            compatibility: 'ie7',
+            keepBreaks: true,
+            keepSpecialComments: '*'
+        }))
+        .pipe(gulp.dest('dist/css'));
 });
